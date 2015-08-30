9328c74bbc04357c05cc848cf939d13fc66e6b9b
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-08-29 22:59:40 -0500

    Created the first urls to enable the usage of the views, also added the functionality to access the models from the administrative panel, just in case.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Categoria, Articulo
 
-# Register your models here.
+
+@admin.register(Articulo)
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'creado_en', 'estado']
+    search_fields = ['titulo', ]
+
+admin.site.register(Categoria)
