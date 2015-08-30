500a4cc6b58602fc026618d9f272e24cb22ab7f6
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-08-29 22:47:40 -0500

    Se añaden las primeras vistas de la aplicacion blog, usando el paradigma de Class Based Views.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from braces.views import LoginRequiredMixin
+from .models import Articulo, Categoria
 
-# Create your views here.
+"""
+## TODO ##
+1. Hace falta la vista que permita editar los articulos creados para que sean
+actualizados.
+2. Hace falta crear vistas para CRUD de las categorias.
+3. Crear mensajes de advertencia sobre el resultado de realizar cualquier
+actividad de CRUD de cada una de las vistas, cada vez que haya una llamada a
+'success_url' en cualquiera de las vistas.
+"""
+
+
+class ListaArticulos(ListView):
+    """Vista usando concepto de 'Class Based Views' para llamar todos los
+    articulos, asi se habilita la opcion de permitir acceso a las
+    funcionalidades del portal, sin necesidad de dar acceso al lado
+    administrativo del mismo.
+    """
+    model = Articulo
+    queryset = Articulo.objects.get_published()
+    paginate_by = 10
+    context_object_name = 'lista_articulos'
+
+
+class DetalleArticulo(DetailView):
+    """Vista usando concepto de 'Class Based Views' para llamar un articulo,
+    asi se habilita la opcion de permitir acceso a las funcionalidades del
+    portal, sin necesidad de dar acceso al lado administrativo del mismo.
+    """
+    model = Articulo
+    context_obj_name = 'articulo'
+
+
+class CrearArticulo(LoginRequiredMixin, CreateView):
+    """Vista usando concepto de 'Class Based Views' para crear un articulo,
+    asi se habilita la opcion de permitir acceso a las funcionalidades del
+    portal, sin necesidad de dar acceso al lado administrativo del mismo.
+    """
+    model = Articulo
+    success_url = '/blog/'
+    fields = ['titulo', 'imagen_destacada', 'contenido', 'categoria', 'autor',
+              'tags', 'estado']
+
+
+class CrearCategoria(LoginRequiredMixin, CreateView):
+    """Vista usando concepto de 'Class Based Views' para crear una categoria,
+    asi se habilita la opcion de permitir acceso a las funcionalidades del
+    portal, sin necesidad de dar acceso al lado administrativo del mismo.
+    """
+    model = Categoria
+    success_url = '/blog/'
