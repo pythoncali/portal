cb89f660b950b29941d716aaec186e3129ed5a68
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-08-31 05:51:59 -0500

    Forum app added, includes basic views using Class Based Views paradigm, basic URL's resolving and the basic models

--- config/settings/common.py
+++ config/settings/common.py
@@ -46,6 +46,7 @@ THIRD_PARTY_APPS = (
 LOCAL_APPS = (
     'djangocali-portal.users',  # custom users app
     # Your stuff: custom apps go here
+    'forum',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
