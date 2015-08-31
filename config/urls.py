1621005c4edec403ffe4e076b5bc7f4865967e73
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-08-31 06:05:26 -0500

    Added the url's resolving, also added some basic template functionality, still pending to define if it works, also needs to implement a nav bar

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-
+    url(r'^blog/', include('blog.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
