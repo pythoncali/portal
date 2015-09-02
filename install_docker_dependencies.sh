4d60d47e19965c601eb7bec546bbbf35300f3e8f
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-09-02 10:17:38 -0500

    Fixing some errors on provision file, and included the docker installing script

---
+++ install_docker_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Add Docker PPA and install latest version
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+apt-get update
+apt-get install lxc-docker -y
+
+# Install docker-compose
+sh -c "curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod +x /usr/local/bin/docker-compose
+sh -c "curl -L https://raw.githubusercontent.com/docker/compose/1.4.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+# Install docker-cleanup command
+cd /tmp
+git clone https://gist.github.com/76b450a0c986e576e98b.git
+cd 76b450a0c986e576e98b
+mv docker-cleanup /usr/local/bin/docker-cleanup
+chmod +x /usr/local/bin/docker-cleanup
--- provision.sh
+++ provision.sh
@@ -1,65 +1,50 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Use Python 3.4
-alias python='/usr/bin/python3.4'
-export PYTHONPATH=/usr/lib/python3.4
-
-# Install git for version control, pip for install python packages
-echo 'Installing python3-pip...'
-apt-get update
-apt-get install -y python3-pip
-
-# Install virtualenv
-echo 'Installing virtualenv'
-alias python='/usr/bin/python3.4'
-pip3 install virtualenv
-mkdir ~vagrant/.virtualenvs
-chown vagrant:vagrant ~vagrant/.virtualenvs
-printf "\n\n# Virtualenv settings\n" >> ~vagrant/.bashrc
-printf "alias python='/usr/bin/python3.4'\n" >> ~vagrant/.bashrc
-virtualenv ~vagrant/.virtualenvs/env
-source ~vagrant/.virtualenvs/env/bin/activate
-
-# #Install packages
-echo 'Configuring language'
+# Setup localizations
+echo 'Configuring localizations'
 echo 'export LANGUAGE="en_US.UTF-8"' | sudo tee -a /etc/profile.d/lang.sh
 echo 'export LANG="en_US.UTF-8"' | sudo tee -a /etc/profile.d/lang.sh
 echo 'export LC_ALL="en_US.UTF-8"' | sudo tee -a /etc/profile.d/lang.sh
 . /etc/profile.d/lang.sh
-
 locale-gen en_US.UTF-8
 sudo dpkg-reconfigure locales
 
-apt-get -y install python3-dev python3-software-properties
-apt-get install -y postgresql-9.3 postgresql-server-dev-9.3
-apt-get install -y libjpeg-dev zlib1g-dev
-apt-get install -y vim gettext memcached libmemcached-dev
+# Install git for version control, pip for install python packages
+echo 'Installing pip & virtualenv...'
+apt-get update
+apt-get install -y python3-pip python-virtualenv python-pip
+apt-get install -y postgresql libpq-dev
 
+echo 'Configuring the project virtual enviroment'
+mkdir ~vagrant/.virtualenvs
+chown vagrant:vagrant ~vagrant/.virtualenvs
+virtualenv --python=python3 --no-site-packages ~vagrant/.virtualenvs/env
+source ~vagrant/.virtualenvs/env/bin/activate
 
-echo "Configure Postgres DB"
+# Postgres Setup
+echo "Configuring Postgres DATABASE"
 sudo -u postgres psql postgres -U postgres -c "CREATE ROLE db_user WITH LOGIN ENCRYPTED PASSWORD 'password' CREATEDB CREATEROLE REPLICATION SUPERUSER"
 sudo -u postgres psql postgres -U postgres -c "CREATE DATABASE my_db"
 sudo -u postgres psql postgres -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE my_db TO db_user"
-
-
 echo "listen_addresses = '*' " >> /etc/postgresql/9.3/main/postgresql.conf
 echo "host    all    all    all    password" >> /etc/postgresql/9.3/main/pg_hba.conf
-
 sudo service postgresql restart
 
 #Install the python requirements (inside the virtualenv)
+# Config virtualenv
+
 cd /vagrant/
+./install_os_dependencies.sh install
+./install_docker_dependencies.sh
 echo "Installing requirements for python"
-pip3 install -r requirements/local.txt
-sudo chown -R vagrant /home/vagrant/.virtualenvs
+pip install -r requirements/local.txt
+sudo chown -R vagrant:vagrant ~vagrant/.virtualenvs/
+
 touch config/settings/.env
 echo "DATABASE_URL=postgresql://db_user:password@127.0.0.1:5432/my_db" >> config/settings/.env
 
 echo "Setting defaults for when using ssh"
 echo "source ~vagrant/.virtualenvs/env/bin/activate" >> ~vagrant/.bashrc
 echo "cd /vagrant" >> ~vagrant/.bashrc
-sudo chown -R vagrant:vagrant ~vagrant/.virtualenvs/
-
-python3 manage.py migrate
 
 echo "Project setup finished."
