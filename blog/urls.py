2d173b25733b15545e8ba58e2d70e24962d94fa1
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-09-07 16:04:00 -0500

    Fixed bad template locations, and added the Category management with views and templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import url
 from .views import (ListaArticulos, DetalleArticulo, CrearArticulo,
-                    CrearCategoria)
+                    CrearCategoria, ListaCategorias)
 
 # Crear URL's para vistas CrearArticulo y EditarArticulo
 
 urlpatterns = [
     url(r'^$', ListaArticulos.as_view(), name="lista_articulos"),
     url(r'^crear-articulo/$', CrearArticulo.as_view(), name='crear_articulo'),
-    url(r'^(?P<slug>[-\w]+)/$', DetalleArticulo.as_view(),
-        name="detalle_articulo"),
     url(r'^crear-categoria/$', CrearCategoria.as_view(),
         name='crear_categoria'),
+    url(r'^categorias/$', ListaCategorias.as_view(),
+        name="lista_categorias"),
+    url(r'^entradas/(?P<slug>[-\w]+)/$', DetalleArticulo.as_view(),
+        name="detalle_articulo"),
 ]
