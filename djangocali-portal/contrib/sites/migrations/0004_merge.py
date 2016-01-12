a0ca244f7e63ecc990314020fc0ea2ceabbdfcf6
andresgz <andresgz@gmail.com>
2016-01-12 17:02:09 -0500

    #38 issue with migrations

--- djangocali-portal/contrib/sites/migrations/0004_merge.py
+++ djangocali-portal/contrib/sites/migrations/0004_merge.py
@@ -7,8 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sites', '0003_auto_20150830_0409'),
-        ('sites', '0003_auto_20150831_1054'),
+        ('sites', '0003_auto_20150905_2016'),
     ]
 
     operations = [
