ca1f49073fcb8e4480270dfd77f2ac83335b05a4
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2016-02-03 15:11:48 -0500

    Adding some test to validate the urls and the views

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import reverse
 from .models import Articulo, Categoria
+
 from model_mommy import mommy
 
 
@@ -33,3 +34,38 @@ class TestModels(TestCase):
         self.assertTrue(Articulo.objects.get_published())
         self.assertEqual(
             Articulo.objects.get_published()[0].titulo, articulo.titulo)
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.art_uno = mommy.make(Articulo, estado="p")
+        self.art_dos = mommy.make(Articulo, estado="p")
+
+    def test_lista_articulos(self):
+        url = reverse('lista_articulos')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(
+            self.art_uno.categoria, self.art_dos.categoria in resp.context[
+                "categorias_list"])
+
+    def test_detalle_articulo(self):
+        url_uno = reverse('detalle_articulo', args=[self.art_uno.slug])
+        resp_uno = self.client.get(url_uno)
+        self.assertEqual(resp_uno.status_code, 200)
+        self.assertTrue(
+            self.art_uno.titulo in resp_uno.context["articulo"].__str__())
+        url_dos = reverse('detalle_articulo', args=[self.art_dos.slug])
+        resp_dos = self.client.get(url_dos)
+        self.assertEqual(resp_dos.status_code, 200)
+        self.assertTrue(
+            self.art_dos.titulo in resp_dos.context["articulo"].__str__())
+
+    def test_lista_categorias(self):
+        url = reverse('lista_categorias')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(
+            self.art_uno.categoria, self.art_dos.categoria in resp.context[
+                "lista_categorias"])
